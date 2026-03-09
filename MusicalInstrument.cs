210eb5a0ba71347fac17eb01461896394d927476
ipz245skv-sketch <ipz245_skv@student.ztu.edu.ua>
2026-03-09 23:06:18 +0200

    MusicalInstrument.cs

--- MusicalInstrument.cs
+++ MusicalInstrument.cs
@@ -7,14 +7,12 @@ namespace Lab1.Classes
         protected string Name; 
         protected string Characteristics; 
 
-        // Конструктор за замовчуванням
         public MusicalInstrument() 
         { 
             Name = "Невідомий інструмент"; 
             Characteristics = "Без характеристик"; 
         }
 
-        // Конструктори з параметрами
         public MusicalInstrument(string name) 
         { 
             Name = name; 
@@ -26,25 +24,20 @@ namespace Lab1.Classes
             Name = name; 
             Characteristics = characteristics; 
         }
-
-        // Конструктор копіювання
+        
         public MusicalInstrument(MusicalInstrument other) 
         { 
             Name = other.Name; 
             Characteristics = other.Characteristics; 
         }
-
-        // Абстрактні методи (мають бути реалізовані в нащадках)
         public abstract void Sound();
         public abstract void Show();
         public abstract void Desc();
         public abstract void History();
 
-        // Методи встановлення та читання
         public void SetName(string name) => Name = name;
         public string GetName() => Name;
 
-        // Метод виводу всієї інформації
         public virtual void ShowInfo()
         {
             Console.WriteLine($"Назва: {Name}");
